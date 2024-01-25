08f310a247b4f4ae3589d9769f263ae7ed86bb6f
Kianush <150435197+KiaMon@users.noreply.github.com>
2024-01-25 20:33:53 +0100

    Add files via upload

---
+++ remind_me.R
@@ -0,0 +1,10 @@
+remind_me <- function() {
+  animals_that_outrank_me <-
+    c("Nils Olav", "King Neptune", "Sergeant Major Fosco", 
+      "Staff Sergeant Reckless", "Sergeant Chesty XIV", "Sergeant Major Jiggs", 
+      "Lance Corporal Billy Windsor", "Sinbad, the Chief Dog", 
+      "Master Sergeant Big Deuce VI", "Corporal Short Round V")
+  return(animals_that_outrank_me)
+}
+
+remind_me()
